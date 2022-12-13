0e0d21ce704bdf84ae718414b161652b78661943
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-13 07:39:01 +0000

    Adding tarfile member sanitization to extractall()

--- prerequisites/download_images.py
+++ prerequisites/download_images.py
@@ -104,7 +104,26 @@ def download_unrel(data_folder):
     os.system("wget " + UNREL)
     shutil.move('unrel-dataset.tar.gz', data_folder + 'unrel-dataset.tar.gz')
     with tarfile.open(data_folder + 'unrel-dataset.tar.gz') as fid:
-        fid.extractall(data_folder)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(fid, data_folder)
     os.remove(data_folder + 'unrel-dataset.tar.gz')
     # Move all images to specified path
     for name in os.listdir(data_folder + 'sg_dataset/sg_train_images/'):
